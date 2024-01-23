6945913fa2e8e0e88f5c9df420236a5ef0dc9277
Sara Antonuccio <sararaynea@gmail.com>
2024-01-23 15:41:58 -0500

    add initial pages and set up app

---
+++ app/Library/Library.page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Library = () => {
+  return (
+    <div>This is the library page</div>
+  );
+}
+
+export default Library();
---
+++ app/Library/playing/playing.page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Library = () => {
+  return (
+    <div>
+      This is the currently playing page nested in the library page
+      <div>localhost:3000/library/playing</div>
+      <div>add more for played and plan to play</div>
+    </div>
+  );
+}
+
+export default Library();
